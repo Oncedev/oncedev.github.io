9c568177b449e072302ff63a2c6375f3601a784d
Luiz Romário Santana Rios <luizromario@gmail.com>
2014-06-05 15:08:16 -0300

    Fazer o script parar de executar em caso de erro

--- jekyll-build.sh
+++ jekyll-build.sh
@@ -1,17 +1,17 @@
-jekyll build
-git checkout master
+jekyll build &&
+git checkout master &&
 
 for i in *; do
   if [[ $i != "_site" ]]; then
     rm -rf $i
   fi
-done
+done &&
 
-cp _site/* . -r
-rm -rf _site
-touch .nojekyll
+cp _site/* . -r &&
+rm -rf _site &&
+touch .nojekyll &&
 
-git add --all .
+git add --all . &&
 git commit
 
 git checkout source
