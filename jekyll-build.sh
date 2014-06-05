98aff00e85022070dc615c31073e9594191fb369
Luiz Romário Santana Rios <luizromario@gmail.com>
2014-06-05 14:58:38 -0300

    Update script

---
+++ jekyll-build.sh
@@ -0,0 +1,17 @@
+jekyll build
+git checkout master
+
+for i in *; do
+  if [[ $i != "_site" ]]; then
+    rm -rf $i
+  fi
+done
+
+cp _site/* . -r
+rm -rf _site
+touch .nojekyll
+
+git add --all .
+git commit
+
+git checkout source
